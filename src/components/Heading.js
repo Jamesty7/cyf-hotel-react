c624bb503e2c3ed1f2487dc9265f0a5d12b44aaa
Jamesty7 <jamestysoto@gmail.com>
2022-05-13 21:59:46 +0200

    Lesson 1 from homework

---
+++ src/components/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo"
+      />
+      <h4>CYF Hotel</h4>
+    </header>
+  );
+};
+
+export default Heading;
